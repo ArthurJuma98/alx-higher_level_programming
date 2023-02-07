74a939326ff12c0d91097b20a19e16dc03651693
ArthurJuma98 <arthurjuma98@gmail.com>
2023-02-07 06:11:10 -0800

    Python Input Output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,41 +1,20 @@
-
-"""
-Student definition module
-"""
+#!/usr/bin/python3
+"""Student class"""
 
 
 class Student:
-    """A student definition class"""
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
+    """Student class"""
+
+    def __init__(self, first, last, age):
+        self.first_name = first
+        self.last_name = last
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a ``Student``.
-        Args:
-            attrs (:obj:`list` of `str`, optional): The list of attributes
-                                                    to retrieve.
-        Returns:
-            (`dict`)
-        """
-        d = {}
-        if attrs:
-            for a in attrs:
-                try:
-                    d[a] = self.__getattribute__(a)
-                except Exception:
-                    pass
-        else:
-            d['first_name'] = self.first_name
-            d['last_name'] = self.last_name
-            d['age'] = self.age
-        return d
-
-    def reload_from_json(self, json):
-        """Replaces all attributes of the ``Student`` instance.
-        Args:
-            json (`dict`): The dictionnary of attributes to replace.
-        """
-        for key, value in json.items():
-            setattr(self, key, value)
+    def to_json(self):
+        """Returns student's serializable dict elements as a dict"""
+        retdict = {}
+        objdict = self.__dict__
+        for ele in objdict:
+            if type(objdict[ele]) in [list, dict, str, int, bool]:
+                retdict[ele] = objdict[ele]
+        return retdict
